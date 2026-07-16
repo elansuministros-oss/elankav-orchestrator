9238c10523dc7322e8c20ff98748f76501c49a51
elansuministros-oss <elansuministros@gmail.com>
2026-07-15 18:23:06 -0600

    VQS-01 crea registro central de ejecutivos comerciales

---
+++ config/vqsExecutives.js
@@ -0,0 +1,23 @@
+const VQS_EXECUTIVE_REGISTRY_VERSION = '1.0.0';
+
+const executives = {
+  'EXEC-ERICK-CANO-001': {
+    executiveId: 'EXEC-ERICK-CANO-001',
+    name: 'Erick Cano',
+    role: 'Director Comercial',
+    phone: '+505 8838 8940',
+    email: '',
+    photoUrl: '',
+    platforms: ['ELANVISUAL'],
+    active: true,
+    commissionEligible: false,
+    commissionPolicyId: null,
+    createdAt: '2026-07-15',
+    registryVersion: VQS_EXECUTIVE_REGISTRY_VERSION
+  }
+};
+
+module.exports = {
+  VQS_EXECUTIVE_REGISTRY_VERSION,
+  executives
+};
